58054c00af5f2ca4a5026dc2b823300dff80e3ce
Aysebrr <ayseebraroral@gmail.com>
2023-10-31 19:13:34 +0300

    ayseebrar

--- src/test/java/techproed/stepDefinition/ui_step_defs/US07_Dean_Mesaj_Okur_siler_StepDefs.java
+++
@@ -1,4 +0,0 @@
-package techproed.stepDefinition.ui_step_defs;
-
-public class US07_Dean_Mesaj_Okur_siler_StepDefs {
-}
---
+++ src/test/java/techproed/stepDefinition/ui_step_defs/US23_AdminViceDeanOlusturabilme.java
@@ -0,0 +1,4 @@
+package techproed.stepDefinition.ui_step_defs;
+
+public class US23_AdminViceDeanOlusturabilme {
+}
