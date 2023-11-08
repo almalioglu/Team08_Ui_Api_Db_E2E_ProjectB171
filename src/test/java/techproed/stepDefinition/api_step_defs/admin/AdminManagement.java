c03f2e1d487081313ef350f8b20c6cd7c7fed285
mehmet <mehmetserkanercik@gmail.com>
2023-11-08 19:33:42 +0300

    mehmet-08.11.2023-v11

--- src/test/java/techproed/stepDefinition/api_step_defs/admin/AdminManagement.java
+++ src/test/java/techproed/stepDefinition/api_step_defs/admin/AdminManagement.java
@@ -4,13 +4,19 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import techproed.pojos.admin.addAdmin.AdminPostPojo;
 import techproed.pojos.admin.addAdmin.AdminPostResponsePojo;
+import techproed.pojos.admin.getAdmin.AdminGetExpectedPojo;
+import techproed.pojos.admin.getAdmin.AdminGetResponsePojo;
 import techproed.utilities.ConfigReader;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static techproed.base_url.BaseUrl.setup;
 import static techproed.base_url.BaseUrl.spec;
 
@@ -18,15 +24,18 @@ public class AdminManagement {
     AdminPostPojo payload;
     Response response;
     AdminPostResponsePojo actualData;
+    AdminGetExpectedPojo expected;
+    AdminGetResponsePojo getActualData;
+
     @Given("admin save icin URL duzenlenir")
     public void adminSaveIcinURLDuzenlenir() {
         //https://managementonschools.com/app/admin/save
-        spec.pathParams("first","admin","second","save");
+        spec.pathParams("first", "admin", "second", "save");
     }
 
     @And("admin save icin payload duzenlenir")
     public void adminSaveIcinPayloadDuzenlenir() {
-        payload=new AdminPostPojo("1996-10-02","ADANA",true,"MALE","API","A123456789a","458-972-2431","751-65-8742","DENEME","APIDENEME");
+        payload = new AdminPostPojo("1996-10-02", "ADANA", true, "MALE", "API", "A123456789a", "458-972-2431", "751-65-8742", "DENEME", "APIDENEME");
 
         /*
         {
@@ -46,26 +55,26 @@ public class AdminManagement {
 
     @When("admin save icin post request gonderilir ve response alinir")
     public void adminSaveIcinPostRequestGonderilirVeResponseAlinir() {
-        response=given(spec).body(payload).when().post("{first}/{second}");
-        actualData=response.as(AdminPostResponsePojo.class);
+        response = given(spec).body(payload).when().post("{first}/{second}");
+        actualData = response.as(AdminPostResponsePojo.class);
     }
 
     @Then("status kodun {int} oldugu dogrulanir")
     public void statusKodunOlduguDogrulanir(int statusCode) {
-        assertEquals(statusCode,response.statusCode());
+        assertEquals(statusCode, response.statusCode());
     }
 
     @And("admin save icin gelen response body dogrulanir")
     public void adminSaveIcinGelenResponseBodyDogrulanir() {
-        assertEquals(payload.getUsername(),actualData.getObject().getUsername());
-        assertEquals(payload.getName(),actualData.getObject().getName());
-        assertEquals(payload.getSurname(),actualData.getObject().getSurname());
-        assertEquals(payload.getBirthDay(),actualData.getObject().getBirthDay());
-        assertEquals(payload.getSsn(),actualData.getObject().getSsn());
-        assertEquals(payload.getBirthPlace(),actualData.getObject().getBirthPlace());
-        assertEquals(payload.getPhoneNumber(),actualData.getObject().getPhoneNumber());
-        assertEquals(payload.getGender(),actualData.getObject().getGender());
-        assertEquals(payload.getGender(),actualData.getObject().getGender());
+        assertEquals(payload.getUsername(), actualData.getObject().getUsername());
+        assertEquals(payload.getName(), actualData.getObject().getName());
+        assertEquals(payload.getSurname(), actualData.getObject().getSurname());
+        assertEquals(payload.getBirthDay(), actualData.getObject().getBirthDay());
+        assertEquals(payload.getSsn(), actualData.getObject().getSsn());
+        assertEquals(payload.getBirthPlace(), actualData.getObject().getBirthPlace());
+        assertEquals(payload.getPhoneNumber(), actualData.getObject().getPhoneNumber());
+        assertEquals(payload.getGender(), actualData.getObject().getGender());
+        assertEquals(payload.getGender(), actualData.getObject().getGender());
     }
 
     @Given("admin icin setup parametreleri olusturulur")
@@ -76,42 +85,59 @@ public class AdminManagement {
 
     @Given("{string} yetkisi ile giris yapilir")
     public void yetkisiIleGirisYapilir(String user) {
-        switch (user.toLowerCase()){
+        switch (user.toLowerCase()) {
             case "admin":
                 setup(ConfigReader.getProperty("adminUsername"), ConfigReader.getProperty("Password"));
                 break;
             case "dean":
-                setup(ConfigReader.getProperty("Dean"),ConfigReader.getProperty("Password"));
+                setup(ConfigReader.getProperty("Dean"), ConfigReader.getProperty("Password"));
                 break;
             case "vicedean":
-                setup(ConfigReader.getProperty("Vicedean"),ConfigReader.getProperty("Password"));
+                setup(ConfigReader.getProperty("Vicedean"), ConfigReader.getProperty("Password"));
                 break;
             case "teacher":
-                setup(ConfigReader.getProperty("Teacher"),ConfigReader.getProperty("Password"));
+                setup(ConfigReader.getProperty("Teacher"), ConfigReader.getProperty("Password"));
+                break;
 
         }
     }
 
 
-
     @Given("admin get icin URL duzenlenir")
     public void adminGetIcinURLDuzenlenir() {
         //https://managementonschools.com/app/admin/getAll?page=1&size=10&sort=name&type=desc
-        spec.pathParams("first","admin","second","getAll")
-                .queryParams("first","page=1","second","size=10","third","sort=name","fourth","type=desc");
+        spec.pathParams("first", "admin", "second", "getAll");
     }
 
     @And("admin get expected data duzenlenir")
     public void adminGetExpectedDataDuzenlenir() {
 
+        expected = new AdminGetExpectedPojo(1166, "APIDENEME", "751-65-8742", "API", "DENEME", "1996-10-02", "ADANA", "458-972-2431", "MALE", false);
+    /*
+    {
+            "id": 1166,
+            "username": "APIDENEME",
+            "ssn": "751-65-8742",
+            "name": "API",
+            "surname": "DENEME",
+            "birthDay": "1996-10-02",
+            "birthPlace": "ADANA",
+            "phoneNumber": "458-972-2431",
+            "gender": "MALE",
+            "built_in": false
+        }
+     */
     }
 
     @When("admin get request gonderilir ve response alinir")
     public void adminGetRequestGonderilirVeResponseAlinir() {
-
+        response = given(spec).when().get("{first}/{second}");
+        getActualData=response.as(AdminGetResponsePojo.class);
     }
 
     @And("admin get ile gelen response dogrulanir")
     public void adminGetIleGelenResponseDogrulanir() {
+        assertEquals(expected.getUsername(),getActualData.getContent().get(1).getUsername());
     }
+
 }
