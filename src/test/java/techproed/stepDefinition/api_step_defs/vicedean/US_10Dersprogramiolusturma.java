293afcf991573efae7287d01e1304ab415b2eeb8
Cihan Karanfil <cihankaranfil44@hotmail.com>
2023-11-14 19:23:11 +0300

    Cihan_14.11_V15

--- src/test/java/techproed/stepDefinition/api_step_defs/vicedean/US_10Dersprogramiolusturma.java
+++ src/test/java/techproed/stepDefinition/api_step_defs/vicedean/US_10Dersprogramiolusturma.java
@@ -1,9 +1,11 @@
 package techproed.stepDefinition.api_step_defs.vicedean;
 
 import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import techproed.pojos.vicedean.deletelessonprograms.DeletelessonprogramsPojo;
 import techproed.pojos.vicedean.postLessonProgram.ExpectedDataPojo;
 import techproed.pojos.vicedean.postLessonProgram.ResponsePojo;
 
@@ -13,10 +15,12 @@ import java.util.List;
 import static io.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertEquals;
 import static techproed.base_url.BaseUrl.spec;
+import static techproed.pojos.vicedean.postLessonProgram.ObjectPojo.lessonProgramId;
 
 public class US_10Dersprogramiolusturma {
     ExpectedDataPojo payload;
     Response response;
+    DeletelessonprogramsPojo actualData;
     public static List<Integer> lessonidlist;
 
     Faker fake= new Faker();
@@ -39,6 +43,7 @@ public class US_10Dersprogramiolusturma {
     response.prettyPrint();
 
 
+
     }
 
     @Then("olusturulan ders programi bilgileri dogrulanir")
@@ -46,14 +51,34 @@ public class US_10Dersprogramiolusturma {
         ResponsePojo actualData= response.as(ResponsePojo.class);
         assertEquals(200,response.statusCode());
         assertEquals(payload.getDay(),actualData.getObject().getDay());
-        //assertEquals(payload.getEducationTermId(),actualData.getObject().get);
         // assertEquals(payload.getStudentIds().get(0), String.valueOf(actualData.getObject().getStudents().get(0).getId()));
         assertEquals(payload.getLessonIdList().get(0).toString(),String.valueOf(actualData.getObject().getLessonName().get(0).getLessonId()));
        // assertEquals(payload.getStartTime(),actualData.getObject().getStartTime());
        // assertEquals(payload.getStopTime(),actualData.getObject().getStopTime());
+        System.out.println(lessonProgramId);
 
     }
 
 
+    @Then("silinecek des icin URL duzenlenir")
+    public void silinecekDesIcinURLDuzenlenir() {
+        //{{baseUrl}}/lessonPrograms/delete/:id
+        spec.pathParams("first","lessonPrograms","second","delete","third",lessonProgramId);
     }
+
+    @And("silinecek ders icin DELETE Request gonderilir ve Response alinir")
+    public void silinecekDersIcinDELETERequestGonderilirVeResponseAlinir() {
+        response  = given(spec).when().delete("{first}/{second}/{third}");
+        response.prettyPrint();
+         actualData = response.as(DeletelessonprogramsPojo.class);
+
+    }
+
+    @And("gelen Response body dogrulanir")
+    public void gelenResponseBodyDogrulanir() {
+
+        assertEquals("Lesson Program Deleted",actualData.getMessage());
+        assertEquals("OK",actualData.getHttpStatus());
+    }
+}
 
