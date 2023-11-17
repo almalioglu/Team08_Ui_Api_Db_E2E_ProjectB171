33ce2d17f78577676496f3c601ee9d9cbd02203e
Taha Ahmet Eşer <tahaee78@gmail.com>
2023-11-17 19:24:08 +0300

    Taha-17.11.23-v2

--- src/test/java/techproed/stepDefinition/api_step_defs/vicedean/US08_US09.java
+++ src/test/java/techproed/stepDefinition/api_step_defs/vicedean/US08_US09.java
@@ -25,12 +25,11 @@ public class US08_US09 {
     public static ObjectTEPojo object;
     public static ResponseTEPojo expectedData;
 
-
     @Given("Vice Dean yetkisi ile giris yapilirr")
     public void viceDeanYetkisiIleGirisYapilirr() {
         setup("vicedeanjohn007","qweasdzxcfF2");
     }
-    //Vice Dean lessons save Request gonderilir ve Response alinir
+
     @And("Vice Dean lessons save icin URL düzenlenir")
     public void viceDeanLessonsSaveIcinURLDüzenlenir() {
         spec.pathParams("first","lessons","second","save");
@@ -39,6 +38,13 @@ public class US08_US09 {
     @And("Vice Dean lessons save icin payload duzenlenirr")
     public void viceDeanLessonsSaveIcinPayloadDuzenlenirr() {
         payload = new LessonssPojo("True","5","Algorithm 2");
+    }
+
+    @When("Vice Dean lessons save icin Request gonderilir ve Response alinir")
+    public void viceDeanLessonsSaveRequestGonderilirVeResponseAlinir() {
+        response = given(spec).body(payload).when().post("{first}/{second}");
+        response.prettyPrint();
+        actualData = response.as(ResponseTEPojo.class);
     }
 
     @Then("status kodun {int} oldugu dogrulanirr")
@@ -53,18 +59,17 @@ public class US08_US09 {
         assertEquals(payload.getLessonName(), actualData.getObject().getLessonName());
     }
 
-
     @Given("Kayitli ders bilgisinin ID nosu alinirrr")
     public void kayitliDersBilgisininIDNosuAlinirrr() {
         setup("vicedeanjohn007","qweasdzxcfF2");
         spec.pathParams("first","lessons","second", "getAll");
         response = given(spec).when().get("{first}/{second}");
+
         JsonPath json = response.jsonPath();
         List<Integer> lessonIdList = json.getList("findAll{it.lessonName=='Algorithm 2'}.lessonId");
         lessonId = lessonIdList.get(0);
+
     }
-
-
 
     @And("Lesson getAllLessonByLessonId icin URL duzenlenirrr")
     public void lessonGetAllLessonByLessonIdIcinURLDuzenlenirrr() {
@@ -90,37 +95,5 @@ public class US08_US09 {
         assertEquals(object.isCompulsory(), actualData.getObject().isCompulsory());
     }
 
-
-    @When("Vice Dean lessons save Request gonderilir ve Response alinir")
-    public void viceDeanLessonsSaveRequestGonderilirVeResponseAlinir() {
-        response = given(spec).body(payload).when().post("{first}/{second}");
-        response.prettyPrint();
-        actualData = response.as(ResponseTEPojo.class);
-
-    }
-
-    @And("Vice Dean lessons get all için name alınır")
-    public void viceDeanLessonsGetAllIçinNameAlınır() {
-        
-    }
-
-    @When("Vice Dean lessons get Lesson by name için URL duzenlenir")
-    public void viceDeanLessonsGetLessonByNameIçinURLDuzenlenir() {
-
-    }
-
-    @And("Vice Dean lessons get Lesson by name için Expected Data duzenlenir")
-    public void viceDeanLessonsGetLessonByNameIçinExpectedDataDuzenlenir() {
-
-    }
-
-    @And("Vice Dean lessons get Lesson by name için Request gonderilir ve Response alinir")
-    public void viceDeanLessonsGetLessonByNameIçinRequestGonderilirVeResponseAlinir() {
-
-    }
-
-    @And("Vice Dean lessons get Lesson by name için gelen Response body dogrulanir")
-    public void viceDeanLessonsGetLessonByNameIçinGelenResponseBodyDogrulanir() {
-    }
 }
 
